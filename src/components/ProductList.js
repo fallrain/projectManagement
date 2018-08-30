1026af5d43c2931e21faab58686233bd48a4a1be
王广开 <416266475@qq.com>
2018-08-30 16:06:47 +0800

    products

---
+++ src/components/ProductList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Table, Popconfirm, Button} from 'antd';
+
+const ProductList = ({onDelete, products}) => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name"
+    },
+    {
+      title: "Action",
+      render: (text, record) => {
+        return (
+          <Popconfirm title="删除吗？" onConfirm={() => onDelete(record.id)}>
+            <Button>删除</Button>
+          </Popconfirm>
+        );
+      }
+    }
+  ];
+  return (
+    <Table>
+      dataSource={products}
+      columns={columns}
+    </Table>
+  );
+};
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+export default ProductList;
